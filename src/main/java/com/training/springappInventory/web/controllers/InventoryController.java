24f79c11d99b28da76714a2cac48e591ac492cbd
YAHernandez666 <alcala.yeicob@uabc.edu.mx>
2021-11-29 18:07:07 -0800

    segundo commit

--- src/main/java/com/training/springappInventory/web/controllers/InventoryController.java
+++ src/main/java/com/training/springappInventory/web/controllers/InventoryController.java
@@ -14,6 +14,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 import com.training.springappInventory.business.services.IProductManager;
 
+//controlador para pagina principal del inventario
 @Controller
 public class InventoryController {
 
