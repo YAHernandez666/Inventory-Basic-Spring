6466922507e7d5589be12f534fadecfc6529f95b
YAHernandez666 <alcala.yeicob@uabc.edu.mx>
2021-12-01 02:21:23 -0800

    se agrego el scrip de la bd y el load data

---
+++ db/springappinventoryv2.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE springappinventoryv2;
+
+GRANT ALL ON springappinventoryv2.* TO springappinventoryuser@'%' IDENTIFIED BY 'pspringappinventoryuser';
+GRANT ALL ON springappinventoryv2.* TO springappinventoryuser@localhost IDENTIFIED BY 'pspringappinventoryuser';
+
+USE springappinventoryv2;
+
+CREATE TABLE products (
+  id INTEGER PRIMARY KEY,
+  description varchar(255),
+  price decimal(15,2)
+);
+CREATE INDEX products_description ON products(description);
